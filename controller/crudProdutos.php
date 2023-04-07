f711d4df39ce25e98051bc89b15ee0f090e4c6f4
dsm-3 <di.silvargt@gmail.com>
2023-04-06 21:05:01 -0300

    atualização crud produto

--- controller/crudProdutos.php
+++ controller/crudProdutos.php
@@ -1,8 +1,9 @@
 <?php
-// Arquivo de conexão com o banco
 require '../model/database.php';
+use MongoDB\Driver\Manager;
+use MongoDB\BSON\ObjectId;
+use MongoDB\Driver\Exception\Exception;
 
-// Função para criar um novo produto
 function criarProduto($nome, $preco, $descricao, $quantidade)
 {
     global $colecao;
@@ -16,15 +17,19 @@ function criarProduto($nome, $preco, $descricao, $quantidade)
     return $resultado->getInsertedId();
 }
 
-// Função para buscar um produto pelo ID
-function buscarProduto($id)
+function buscarProdutoPorNome($nome)
 {
-    global $colecao;
-    $resultado = $colecao->findOne(["_id" => new MongoDB\BSON\ObjectId($id)]);
-    return $resultado;
+	try {
+		$manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+		$filter = ['nome' => $nome];
+		$query = new MongoDB\Driver\Query($filter);
+		$cursor = $manager->executeQuery('bichochique_db.produtos', $query);
+		// Resto do código aqui...
+	} catch (MongoDB\Driver\Exception\Exception $e) {
+		echo "Erro ao buscar produto por nome: " . $e->getMessage();
+	}
 }
 
-// Função para atualizar um produto pelo ID
 function atualizarProduto($id, $nome, $preco, $descricao, $quantidade)
 {
     global $colecao;
@@ -40,7 +45,6 @@ function atualizarProduto($id, $nome, $preco, $descricao, $quantidade)
     return $resultado->getModifiedCount();
 }
 
-// Função para deletar um produto pelo ID
 function deletarProduto($id)
 {
     global $colecao;
@@ -48,29 +52,12 @@ function deletarProduto($id)
     return $resultado->getDeletedCount();
 }
 
-// Exemplo de uso das funções
-
-// Cria um novo produto
 $idProduto = criarProduto("Produto 1", 10.99, "Descrição do Produto 1", 1);
 echo "ID do Produto criado: " . $idProduto . "<br>";
 
-// Busca um produto pelo ID
-$produto = buscarProduto($idProduto);
-echo "Produto buscado: ";
-print_r($produto);
-echo "<br>";
-
-// Atualiza um produto pelo ID
 $produtoAtualizado = atualizarProduto($idProduto, "Produto 1 Atualizado", 15.99, "Nova descrição do Produto 1", 2);
 echo "Número de produtos atualizados: " . $produtoAtualizado . "<br>";
 
-// Busca novamente o produto pelo ID para verificar a atualização
-$produtoAtualizado = buscarProduto($idProduto);
-echo "Produto atualizado: ";
-print_r($produtoAtualizado);
-echo "<br>";
-
-// Deleta um produto pelo ID
 $produtoDeletado = deletarProduto($idProduto);
 echo "Número de produtos deletados: " . $produtoDeletado . "<br>";
 ?>
--- view/cadastroProduto.php
+++ view/cadastroProduto.php
@@ -1,5 +1,4 @@
 <?php include("blades/header.php");
-
 session_start();
 
 // VERIFICA SE O USUÁRIO ESTÁ AUTENTICADO AO TENTAR ACESSAR UMA PÁGINA PROTEGIDA
@@ -10,7 +9,6 @@ if (!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] != 'SIM') {
 ?>
 
 <?php
-// IMPORTA OS ARQUIVOS DE CONEXÃO E CRUD DOS PRODUTOS
 require '../model/database.php';
 require '../controller/crudProdutos.php';
 
@@ -23,71 +21,96 @@ function limparEntrada($dados)
 	return $dados;
 }
 
-// Verifica se o formulário de cadastro foi enviado
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["cadastrar"])) {
-	$nome = limparEntrada($_POST["nome"]);
-	$preco = floatval(limparEntrada($_POST["preco"]));
-	$descricao = limparEntrada($_POST["descricao"]);
-	$quantidade = limparEntrada($_POST["quantidade"]);
+// Verifica se o formulário foi enviado
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	if (isset($_POST["busca"])) { // Verifica se a variável $_POST["busca"] está definida
+		$palavraChave = limparEntrada($_POST["busca"]);
+		$resultadoBusca = buscarProdutoPorNome($palavraChave);
+		if (!empty($resultadoBusca)) {
+			echo "Resultado da busca:<br>";
+			foreach ($resultadoBusca as $produto) {
+				echo "ID: " . $produto['id'] . "<br>";
+				echo "Nome: " . $produto['nome'] . "<br>";
+				echo "Preço: " . $produto['preco'] . "<br>";
+				echo "Descrição: " . $produto['descricao'] . "<br>";
+				echo "Quantidade: " . $produto['quantidade'] . "<br>";
+				echo "<br>";
+			}
+		} else {
+			echo "Nenhum produto encontrado com a palavra-chave informada.";
+		}
+	} elseif (isset($_POST["cadastrar"]) || isset($_POST["atualizar"])) {
+		$id = "";
+		if (isset($_POST["atualizar"])) {
+			$id = limparEntrada($_POST["id"]);
+		}
+		$nome = limparEntrada($_POST["nome"]);
+		$preco = floatval(limparEntrada($_POST["preco"]));
+		$descricao = limparEntrada($_POST["descricao"]);
+		$quantidade = limparEntrada($_POST["quantidade"]);
 
-	// Insere o novo produto no banco de dados
-	$idProduto = criarProduto($nome, $preco, $descricao, $quantidade);
-	if ($idProduto) {
-		echo "Produto cadastrado com sucesso. ID do Produto: " . $idProduto;
-	} else {
-		echo "Erro ao cadastrar o produto.";
+		if (isset($_POST["cadastrar"])) {
+			$idProduto = criarProduto($nome, $preco, $descricao, $quantidade);
+			if ($idProduto) {
+				echo "Produto cadastrado com sucesso. ID do Produto: " . $idProduto;
+			} else {
+				echo "Erro ao cadastrar o produto.";
+			}
+		} elseif (isset($_POST["atualizar"])) {
+			$produtoAtualizado = atualizarProduto($id, $nome, $preco, $descricao, $quantidade);
+			echo "Produto atualizado com sucesso. Número de produtos atualizados:";
+		}
+	} elseif (isset($_POST["excluir"])) {
+		$id = limparEntrada($_POST["id"]);
+		$produtoDeletado = deletarProduto($id);
+		if ($produtoDeletado) {
+			echo "Produto deletado com sucesso.";
+		} else {
+			echo "Erro ao deletar o produto.";
+		}
 	}
 }
-
-// Verifica se o formulário de edição foi enviado
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["atualizar"])) {
-	$id = limparEntrada($_POST["id"]);
-	$nome = limparEntrada($_POST["nome"]);
-	$preco = floatval(limparEntrada($_POST["preco"]));
-	$descricao = limparEntrada($_POST["descricao"]);
-	$quantidade = limparEntrada($_POST["quantidade"]);
-
-	// Atualiza o produto no banco de dados
-	$produtoAtualizado = atualizarProduto($id, $nome, $preco, $descricao, $quantidade);
-	echo "Produto atualizado com sucesso. Número de produtos atualizados: " . $produtoAtualizado . "<br>";
-}
-
-// Verifica se o formulário de exclusão foi enviado
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["excluir"])) {
-	$id = limparEntrada($_POST["id"]);
-
-	// Deleta o produto do banco de dados
-	$produtoDeletado = deletarProduto($id);
-	echo "Produto deletado com sucesso. Número de produtos deletados: " . $produtoDeletado . "<br>";
-}
-
 ?>
 
-<!-- Formulário de Cadastro -->
+<h1>Gerenciamento de Produtos</h1>
+<h2>Buscar Produtos</h2>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+	<input type="text" name="busca" placeholder="Digite uma palavra-chave">
+	<input type="submit" name="buscar" value="Buscar">
+</form>
+
 <h2>Cadastrar Produto</h2>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-	Nome: <input type="text" name="nome" required><br>
-	Preço: <input type="text" step="0.01" name="preco" required><br>
-	Descrição: <input type="text" name="descricao" required><br>
-	Quantidade: <input type="number" name="quantidade" required><br>
+	<label>Nome do Produto:</label><br>
+	<input type="text" name="nome"><br>
+	<label>Preço do Produto:</label><br>
+	<input type="text" name="preco"><br>
+	<label>Descrição do Produto:</label><br>
+	<textarea name="descricao"></textarea><br>
+	<label>Quantidade do Produto:</label><br>
+	<input type="number" name="quantidade"><br>
 	<input type="submit" name="cadastrar" value="Cadastrar">
 </form>
 
-<!-- Formulário de Edição -->
-<h2>Editar Produto</h2>
+<h2>Atualizar Produto</h2>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-	ID: <input type="text" name="id" required><br>
-	Nome: <input type="text" name="nome" required><br>
-	Preço: <input type="text" step="0.01" name="preco" required><br>
-	Descrição: <input type="text" name="descricao" required><br>\
-	Quantidade: <input type="number" name="quantidade" required><br>\
+	<label>ID do Produto:</label><br>
+	<input type="number" name="id"><br>
+	<label>Nome do Produto:</label><br>
+	<input type="text" name="nome"><br>
+	<label>Preço do Produto:</label><br>
+	<input type="text" name="preco"><br>
+	<label>Descrição do Produto:</label><br>
+	<textarea name="descricao"></textarea><br>
+	<label>Quantidade do Produto:</label><br>
+	<input type="number" name="quantidade"><br>
 	<input type="submit" name="atualizar" value="Atualizar">
 </form>
 
-<!-- Formulário de Exclusão -->
 <h2>Excluir Produto</h2>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-	ID: <input type="text" name="id" required><br>
+	<label>ID do Produto:</label><br>
+	<input type="number" name="id"><br>
 	<input type="submit" name="excluir" value="Excluir">
 </form>
 
