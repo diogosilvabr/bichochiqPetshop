063d7916bcd99085f344b0b301f698040a8e321a
dioguera <di.silvargt@gmail.com>
2023-04-23 21:57:28 -0300

    Update dashboardProduto.php

--- view/dashboardProduto.php
+++ view/dashboardProduto.php
@@ -48,77 +48,79 @@
 					<div class="form-group">
 						<input class="form-control" type="text" name="busca" placeholder="Digite uma palavra-chave">
 					</div>
-					<input class="btn btn-primary" type="submit" name="buscar" value="Buscar">
+					<div class="form-group">
+						<input class="btn btn-primary" type="submit" name="buscar" value="Buscar">
+					</div>
 				</form>
 			</div>
-		<?php
-		// Verifica se o formulário foi enviado
-		if ($_SERVER["REQUEST_METHOD"] == "POST") {
-			if (isset($_POST["busca"])) { // Verifica se a variável $_POST["busca"] está definida
-				$palavraChave = limparEntrada($_POST["busca"]);
-				$resultadoBusca = buscarProdutoPorNome($palavraChave); ?>
-				<div class="table-responsive">
-					<table class="table table-striped">
-						<thead>
-							<tr>
-								<th>ID</th>
-								<th>Nome</th>
-								<th>Preço</th>
-								<th>Descrição</th>
-								<th>Tamanho</th>
-								<th>Quantidade</th>
-								<th>Espécie</th>
-								<th>Categoria</th>
-							</tr>
-						</thead>
-						<tbody>
-				<?php
-					if (!empty($resultadoBusca)) {
-						foreach ($resultadoBusca as $produto) {
-					?>
-					<tr>
-						<td><?php echo $produto['_id']; ?></td>
-						<td><?php echo $produto['nome']; ?></td>
-						<td><?php echo $produto['preco']; ?></td>
-						<td><?php echo $produto['descricao']; ?></td>
-						<td><?php echo $produto['tamanho']; ?></td>
-						<td><?php echo $produto['quantidade']; ?></td>
-						<td><?php echo implode(",", $produto['especie']); ?></td>
-						<td><?php echo $produto['categoria']; ?></td>
-						<td>
-							<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-								<input type="hidden" name="id" value="<?php echo $produto['_id']; ?>">
-								<input class="btn btn-danger"type="submit" name="excluir" value="Excluir">
-							</form>
-						</td>
-						<td>
-							<form method="post" action="updateProduto.php">
-								<input type="hidden" name="id" value="<?php echo $produto['_id']; ?>">
-								<input class="btn btn-success"type="submit" value="Editar">
-							</form>
-							
-						</td>
-					</tr>
-					<?php
+			<?php
+			// Verifica se o formulário foi enviado
+			if ($_SERVER["REQUEST_METHOD"] == "POST") {
+				if (isset($_POST["busca"])) { // Verifica se a variável $_POST["busca"] está definida
+					$palavraChave = limparEntrada($_POST["busca"]);
+					$resultadoBusca = buscarProdutoPorNome($palavraChave); ?>
+					<div class="table-responsive">
+						<table class="table table-striped">
+							<thead>
+								<tr>
+									<th>ID</th>
+									<th>Nome</th>
+									<th>Preço</th>
+									<th>Descrição</th>
+									<th>Tamanho</th>
+									<th>Quantidade</th>
+									<th>Espécie</th>
+									<th>Categoria</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php
+								if (!empty($resultadoBusca)) {
+									foreach ($resultadoBusca as $produto) {
+								?>
+										<tr>
+											<td><?php echo $produto['_id']; ?></td>
+											<td><?php echo $produto['nome']; ?></td>
+											<td><?php echo $produto['preco']; ?></td>
+											<td><?php echo $produto['descricao']; ?></td>
+											<td><?php echo $produto['tamanho']; ?></td>
+											<td><?php echo $produto['quantidade']; ?></td>
+											<td><?php echo implode(",", $produto['especie']); ?></td>
+											<td><?php echo $produto['categoria']; ?></td>
+											<td>
+												<form method="post" action="updateProduto.php">
+													<input type="hidden" name="id" value="<?php echo $produto['_id']; ?>">
+													<input class="btn btn-success" type="submit" value="Editar">
+												</form>
+											</td>
+											<td>
+												<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+													<input type="hidden" name="id" value="<?php echo $produto['_id']; ?>">
+													<input class="btn btn-danger" type="submit" name="excluir" value="Excluir">
+												</form>
+
+											</td>
+										</tr>
+								<?php
+									}
+								}
+								?>
+							</tbody>
+						</table>
+					</div> <?php
+						} else {
+							echo "Nenhum produto encontrado com a palavra-chave informada.";
 						}
 					}
-					?>
-				</tbody>
-			</table>
-		</div> <?php
-				} else {
-					echo "Nenhum produto encontrado com a palavra-chave informada.";
-				}
-			}
-				if (isset($_POST["excluir"])) {
-					$id = limparEntrada($_POST["id"]);
-					$produtoDeletado = deletarProduto($id);
-					if ($produtoDeletado) {
-					echo "Produto deletado com sucesso.";
+					if (isset($_POST["excluir"])) {
+						$id = limparEntrada($_POST["id"]);
+						$produtoDeletado = deletarProduto($id);
+						if ($produtoDeletado) {
+							echo "Produto deletado com sucesso.";
 						} else {
-					echo "Erro ao deletar o produto.";
+							echo "Erro ao deletar o produto.";
+						}
 					}
-				}
-		?>
-		<script src="../vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
-		<?php include("blades/footer.php"); ?>
+							?>
+			<script src="../vendor/twbs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+			<?php include("blades/footer.php"); ?>
